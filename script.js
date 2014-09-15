58452f6963d25d10154ccab4b4f2ff7ab3e30b6c
Michael Nguyen <mln6406@rit.edu>
2014-09-14 22:15:25 -0400

    Putting in the Low fidelity home page coded mockup from Morgan.

---
+++ script.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    $('#about').click(function () {
+        scrollToElement('#footer',1000, -125);
+    });
+});
+
+function scrollToElement(selector, time, verticalOffset) {
+    time = typeof(time) != 'undefined' ? time : 1000;
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $(selector);
+    offset = element.offset();
+    offsetTop = offset.top + verticalOffset;
+    $('html, body').animate({
+        scrollTop: offsetTop
+    }, time);
+}
